15503874fd6be52c58fcb9e879978e417adca552
luisfos <luisfos@hotmail.co.uk>
2015-01-13 16:00:57 +0000

    showing window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <SDL.h>
+#include <cstdlib>
 
 int main()
 {
-    std::cout<<"hello world\n";
+    if(SDL_Init(SDL_INIT_VIDEO))
+    {
+        std::cerr<<"error init SDL\n";
+        exit(EXIT_FAILURE);
+    }
+    SDL_Rect screenSize;
+    SDL_GetDisplayBounds(0,&screenSize);
+
+    SDL_Window *window = SDL_CreateWindow(
+                "OpenGL Demo",
+                SDL_WINDOWPOS_CENTERED,
+                SDL_WINDOWPOS_CENTERED,
+                screenSize.w/2,
+                screenSize.h/2,
+                SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+                );
+
+    SDL_Delay(1000);
+
 }
