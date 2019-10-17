3010af25bfd4d01ad8b2ef1e4e089807ca0c7052
Luis Fos <luisf@ws227.london.etc>
2019-10-17 12:34:51 +0100

    subnet oncreated IN OUT script

---
+++ scripts/sop/subnet_OnCreated.py
@@ -0,0 +1,14 @@
+
+n = kwargs['node']
+
+input1 = n.indirectInputs()[0]
+
+n_in = n.createNode("null", "IN")
+n_out = n.createNode("output", "OUT")
+
+
+n_in.setInput(0, input1)
+n_in.setPosition( input1.position() + hou.Vector2(0,-1) )
+#n_in.moveToGoodPosition()
+n_out.setInput(0, n_in)
+n_out.setPosition( n_in.position() + hou.Vector2(0,-8) )
